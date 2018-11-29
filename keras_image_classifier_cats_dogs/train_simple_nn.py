a05e25534a4dd8c128c982c903a6c6c8ebc89038
Steven Williams <steve@repositive.io>
2018-11-29 23:09:32 +0000

    [F] refactor train_simple_nn.py

--- keras_image_classifier_cats_dogs/train_simple_nn.py
+++ keras_image_classifier_cats_dogs/train_simple_nn.py
@@ -31,97 +31,148 @@ ap.add_argument("-l", "--label-bin", required=True,
                 help="path to output label binarizer")
 ap.add_argument("-p", "--plot", required=True,
                 help="path to output accuracy/loss plot")
+ap.add_argument("-i", "--initlr", required=True, type=float,
+                help="initial learning rate")
+ap.add_argument("-e", "--epochs", required=True, type=int,
+                help="number of epochs")
 args = vars(ap.parse_args())
 
-# initialize the data and labels
-print("[INFO] loading images...")
-data = []
-labels = []
 
-# get the image paths and randomly shuffle them
-imagePaths = sorted(list(paths.list_images(args["dataset"])))
-random.seed(42)
-random.shuffle(imagePaths)
+def preprocessing(_args=args):
 
-# loop over the input images
-for imagePath in imagePaths:
-    # load the image, resize the image to be 32x32 pixels (ignoring
-    # aspect ratio), flatten the image into 32x32x3=3072 pixel image
-    # into a list, and store the image in the data list
-    image = cv2.imread(imagePath)
-    image = cv2.resize(image, (32, 32)).flatten()
-    data.append(image)
+    # initialize the data and labels
+    print("[INFO] loading images...")
+    data = []
+    labels = []
 
-    # extract the class label from the image path and update the
-    # labels list
-    label = imagePath.split(os.path.sep)[-2]
-    labels.append(label)
+    # get the image paths and randomly shuffle them
+    imagePaths = sorted(list(paths.list_images(_args["dataset"])))
+    random.seed(42)
+    random.shuffle(imagePaths)
 
-# scale the raw pixel intensities to the range [0, 1]
-data = np.array(data, dtype="float") / 255.0
-labels = np.array(labels)
+    # loop over the input images
+    for imagePath in imagePaths:
+        # load the image, resize the image to be 32x32 pixels (ignoring
+        # aspect ratio), flatten the image into 32x32x3=3072 pixel image
+        # into a list, and store the image in the data list
+        image = cv2.imread(imagePath)
+        image = cv2.resize(image, (32, 32)).flatten()
+        data.append(image)
 
-# partition the data into training and testing splits using 75% of
-# the data for training and the remaining 25% for testing
-(trainX, testX, trainY, testY) = train_test_split(data,
-                                                  labels,
-                                                  test_size=0.25,
-                                                  random_state=42
-                                                  )
+        # extract the class label from the image path and update the
+        # labels list
+        label = imagePath.split(os.path.sep)[-2]
+        labels.append(label)
 
-# convert the labels from integers to vectors (for 2-class, binary
-# classification you should use Keras' to_categorical function
-# instead as the scikit-learn's LabelBinarizer will not return a
-# vector)
-lb = LabelBinarizer()
-trainY = lb.fit_transform(trainY)
-testY = lb.transform(testY)
+    # scale the raw pixel intensities to the range [0, 1]
+    data = np.array(data, dtype="float") / 255.0
+    labels = np.array(labels)
 
-# define the 3072-1024-512-3 architecture using Keras
-model = tf.keras.Sequential()
-model.add(layers.Dense(1024, input_shape=(3072,), activation="sigmoid"))
-model.add(layers.Dense(512, activation="sigmoid"))
-model.add(layers.Dense(len(lb.classes_), activation="softmax"))
+    # partition the data into training and testing splits using 75% of
+    # the data for training and the remaining 25% for testing
+    (trainX, testX, trainY, testY) = train_test_split(data,
+                                                      labels,
+                                                      test_size=0.25,
+                                                      random_state=42
+                                                      )
 
-# initialize our initial learning rate and # of epochs to train for
-INIT_LR = 0.01
-EPOCHS = 75
+    return (trainX, testX, trainY, testY)
 
-# compile the model using SGD as our optimizer and categorical
-# cross-entropy loss (you'll want to use binary_crossentropy
-# for 2-class classification)
-print("[INFO] training network...")
-opt = tf.keras.optimizers.SGD(lr=INIT_LR)
-model.compile(loss="categorical_crossentropy", optimizer=opt,
-              metrics=["accuracy"])
 
-# train the neural network
-H = model.fit(trainX, trainY, validation_data=(testX, testY),
-              epochs=EPOCHS, batch_size=32)
+def convertLabels(split_data):
 
-# evaluate the network
-print("[INFO] evaluating network...")
-predictions = model.predict(testX, batch_size=32)
-print(classification_report(testY.argmax(axis=1),
-                            predictions.argmax(axis=1), target_names=lb.classes_))
+    (trainX, testX, trainY, testY) = split_data
 
-# plot the training loss and accuracy
-N = np.arange(0, EPOCHS)
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(N, H.history["loss"], label="train_loss")
-plt.plot(N, H.history["val_loss"], label="val_loss")
-plt.plot(N, H.history["acc"], label="train_acc")
-plt.plot(N, H.history["val_acc"], label="val_acc")
-plt.title("Training Loss and Accuracy (Simple NN)")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend()
-plt.savefig(args["plot"])
+    # convert the labels from integers to vectors (for 2-class, binary
+    # classification you should use Keras' to_categorical function
+    # instead as the scikit-learn's LabelBinarizer will not return a
+    # vector)
+    lb = LabelBinarizer()
+    trainY_vector = lb.fit_transform(trainY)
+    testY_vector = lb.transform(testY)
 
-# save the model and label binarizer to disk
-print("[INFO] serializing network and label binarizer...")
-model.save(args["model"])
-f = open(args["label_bin"], "wb")
-f.write(pickle.dumps(lb))
-f.close()
+    return (lb, trainY_vector, testY_vector)
+
+
+def constructNetwork(lb, _args=args):
+    # define the 3072-1024-512-3 architecture using Keras
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(1024, input_shape=(3072,), activation="sigmoid"))
+    model.add(layers.Dense(512, activation="sigmoid"))
+    model.add(layers.Dense(len(lb.classes_), activation="softmax"))
+
+    # compile the model using SGD as our optimizer and categorical
+    # cross-entropy loss (you'll want to use binary_crossentropy
+    # for 2-class classification)
+    print("[INFO] training network...")
+    opt = tf.keras.optimizers.SGD(lr=_args["initlr"])
+    model.compile(loss="categorical_crossentropy", optimizer=opt,
+                  metrics=["accuracy"])
+
+    return model
+
+
+def trainNetwork(model, split_data, _epochs=args["epochs"]):
+
+    (trainX, testX, trainY, testY) = split_data
+    # train the neural network
+    H = model.fit(trainX, trainY, validation_data=(testX, testY),
+                  epochs=_epochs, batch_size=32)
+
+    return H
+
+
+def evaluateNetwork(model, split_data, lb, H, _epochs=args["epochs"]):
+
+    (trainX, testX, trainY, testY) = split_data
+
+    # evaluate the network
+    print("[INFO] evaluating network...")
+    predictions = model.predict(testX, batch_size=32)
+    print(classification_report(testY.argmax(axis=1),
+                                predictions.argmax(axis=1), target_names=lb.classes_))
+
+    print("[INFO] plotting training loss and accuracy...")
+    # plot the training loss and accuracy
+    N = np.arange(0, _epochs)
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(N, H.history["loss"], label="train_loss")
+    plt.plot(N, H.history["val_loss"], label="val_loss")
+    plt.plot(N, H.history["acc"], label="train_acc")
+    plt.plot(N, H.history["val_acc"], label="val_acc")
+    plt.title("Training Loss and Accuracy (Simple NN)")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend()
+    plt.savefig(args["plot"])
+    print("[INFO] done evaluating.")
+    return
+
+
+def saveModelToDisk(model, lb, _args=args):
+    # save the model and label binarizer to disk
+    print("[INFO] serializing network and label binarizer...")
+    model.save(_args["model"])
+    f = open(_args["label_bin"], "wb")
+    f.write(pickle.dumps(lb))
+    f.close()
+    return
+
+
+def train_simple_nn(_args=args, _preprocessing=preprocessing, _convertLabels=convertLabels, _constructNetwork=constructNetwork, _trainNetwork=trainNetwork, _evaluateNetwork=evaluateNetwork, _saveModelToDisk=saveModelToDisk):
+
+    split_data = _preprocessing()
+    (trainX, testX, trainY, testY) = split_data
+
+    (lb, trainY_vector, testY_vector) = _convertLabels(split_data)
+    model = _constructNetwork(lb)
+
+    updated_split_data = (trainX, testX, trainY_vector, testY_vector)
+    H = _trainNetwork(model, updated_split_data)
+
+    _evaluateNetwork(model, updated_split_data, lb, H)
+    _saveModelToDisk(model, lb)
+
+
+train_simple_nn()
